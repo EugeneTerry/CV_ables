d74058e63eade8ae5ea31f2aea16cd3f8f544a88
Eugene Terry <eugene.terry73@gmail.com>
2022-03-12 14:10:10 -0600

    continue to make views 01

--- CV_ables_api/views/__init__.py
+++ CV_ables_api/views/__init__.py
@@ -1,10 +1,1 @@
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework import status
-
-from CV_ables_api.models import applicant
-
+from .auth import login_user, register_user
