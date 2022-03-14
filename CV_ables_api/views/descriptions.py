76e0517ff989627a0553f353cde83c697ab6e54a
Eugene Terry <eugene.terry73@gmail.com>
2022-03-14 15:56:35 -0500

    added crud to ed and desc

--- CV_ables_api/views/descriptions.py
+++ CV_ables_api/views/descriptions.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from CV_ables_api.models import Description
+from rest_framework import status
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseServerError
+from CV_ables_api.models import Description, Experience
 from CV_ables_api.serializers import DescriptionSerializer
 
 class DescriptionView(ViewSet):
@@ -11,3 +14,49 @@ class DescriptionView(ViewSet):
             description, many=True, context={'request': request}
             )
         return Response(serializer.data)
+    
+    def create(self, request):
+        discription = Description()
+        experience = Experience.objects.get(pk=request.data['experience_id'])
+        
+        discription.description_text = request.data['description_text']
+        discription.experience = experience
+        
+        try:
+            discription.save()
+            serializer = DescriptionSerializer(discription, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def retrieve(self, request, pk=None):
+        try:
+            # The `2` at the end of the route becomes `pk`
+            discription = Description.objects.get(pk=pk)
+            serializer = DescriptionSerializer(discription, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+        
+    def update(self, request, pk=None):
+        discription = Description.objects.get(pk=pk)
+        experience = Experience.objects.get(pk=request.data['experience_id'])
+        
+        discription.description_text = request.data['description_text']
+        discription.experience = experience
+        discription.save()
+        
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk=None):
+        try:
+            discription = Description.objects.get(pk=pk)
+            discription.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Description.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- CV_ables_api/views/educations.py
+++ CV_ables_api/views/educations.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from CV_ables_api import serializers
-from CV_ables_api.models import Education, education
+from rest_framework import status
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseServerError
+from CV_ables_api.models import Education, Applicant
 from CV_ables_api.serializers import EducationSerializer
 
 class EducationView(ViewSet):
@@ -12,3 +14,45 @@ class EducationView(ViewSet):
             education, many=True, context={'request': request}
             )
         return Response(serializer.data)
+    
+    def create(self, request):
+        education = Education()
+        applicant = Applicant.objects.get(pk=request.data['applicant_id'])
+        
+        education.school_name = request.data['school_name']
+        education.city = request.data['city']
+        education.state = request.data['state']
+        education.diploma = request.data['diploma']
+        education.grad_year = request.data['grad_year']
+        education.applicant = applicant
+        
+        try:
+            education.save()
+            serializer = EducationSerializer(education, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def update(self, request, pk=None):
+        education = Education.objects.get(pk=pk)
+        applicant = Applicant.objects.get(pk=request.data['applicant_id'])
+        
+        education.school_name = request.data['school_name']
+        education.city = request.data['city']
+        education.state = request.data['state']
+        education.diploma = request.data['diploma']
+        education.grad_year = request.data['grad_year']
+        education.applicant = applicant
+        education.save()
+        
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    
+    def retrieve(self, request, pk=None):
+        try:
+            # The `2` at the end of the route becomes `pk`
+            education = Education.objects.get(pk=pk)
+            serializer = EducationSerializer(education, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
