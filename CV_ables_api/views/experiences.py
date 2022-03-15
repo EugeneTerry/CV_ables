0329682e890ce66cab2c067acffa251ccd97f055
Eugene Terry <eugene.terry73@gmail.com>
2022-03-14 20:08:27 -0500

    added crud to missions

--- CV_ables_api/views/experiences.py
+++ CV_ables_api/views/experiences.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from CV_ables_api.models import Experience
+from rest_framework import status
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseServerError
+from CV_ables_api.models import Experience, Jobtype, Applicant
 from CV_ables_api.serializers import ExperienceSerializer
 class ExerienceView(ViewSet):
   def list(self, request):
@@ -9,3 +12,61 @@ class ExerienceView(ViewSet):
     serializer = ExperienceSerializer(
       experience, many=True, context={'request': request})
     return Response(serializer.data)
+  
+  def create(self, request):
+        experience = Experience()
+        applicant = Applicant.objects.get(pk=request.data['applicant_id'])
+        jobtype = Jobtype.objects.get(pk=request.data['jobtype_id'])
+        
+        experience.job_title = request.data['job_title']
+        experience.company = request.data['company']
+        experience.start_yr = request.data['start_yr']
+        experience.end_yr = request.data['end_yr']
+        
+        experience.applicant = applicant
+        experience.job_type = jobtype
+        
+        try:
+            experience.save()
+            serializer = ExperienceSerializer(experience, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+  
+  def retrieve(self, request, pk=None):
+        try:
+            # The `2` at the end of the route becomes `pk`
+            experience = Experience.objects.get(pk=pk)
+            serializer = ExperienceSerializer(experience, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+          
+  def update(self, request, pk=None):
+        experience = Experience.objects.get(pk=pk)
+        applicant = Applicant.objects.get(pk=request.data['applicant_id'])
+        jobtype = Jobtype.objects.get(pk=request.data['jobtype_id'])
+        
+        experience.job_title = request.data['job_title']
+        experience.company = request.data['company']
+        experience.start_yr = request.data['start_yr']
+        experience.end_yr = request.data['end_yr']
+        
+        experience.applicant = applicant
+        experience.job_type = jobtype
+        experience.save()
+        
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+  def destroy(self, request, pk=None):
+      try:
+          experience = Experience.objects.get(pk=pk)
+          experience.delete()
+
+          return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+      except Experience.DoesNotExist as ex:
+          return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+      except Exception as ex:
+          return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- CV_ables_api/views/missions.py
+++ CV_ables_api/views/missions.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ViewSet
-from CV_ables_api.serializers import MissionSerializer
 from rest_framework.response import Response
-from CV_ables_api.models import Mission
+from rest_framework import status
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseServerError
+from CV_ables_api.models import Mission, Jobtype, Applicant, mission
+from CV_ables_api.serializers import MissionSerializer
 
 class MissionView(ViewSet):
     def list(self, request):
@@ -11,3 +14,53 @@ class MissionView(ViewSet):
             mission, many=True, context={'request': request}
             )
         return Response(serializer.data)
+    
+    def create(self, request):
+        mission = Mission()
+        applicant = Applicant.objects.get(pk=request.data['applicant_id'])
+        job_type = Jobtype.objects.get(pk=request.data['job_type_id'])
+        
+        mission.mission_text = request.data['mission_text']
+        
+        mission.applicant = applicant
+        mission.job_type = job_type
+        
+        try:
+            mission.save()
+            serializer = MissionSerializer(mission, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        try:
+            # The `2` at the end of the route becomes `pk`
+            mission = Mission.objects.get(pk=pk)
+            serializer = MissionSerializer(mission, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+        
+    def update(self, request, pk=None):
+        mission = Mission.objects.get(pk=pk)
+        applicant = Applicant.objects.get(pk=request.data['applicant_id'])
+        job_type = Jobtype.objects.get(pk=request.data['job_type_id'])
+        
+        mission.mission_text = request.data['mission_text']
+        mission.applicant = applicant
+        mission.job_type = job_type
+        mission.save()
+        
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk=None):
+        try:
+            mission = Mission.objects.get(pk=pk)
+            mission.delete()
+            
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Mission.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
