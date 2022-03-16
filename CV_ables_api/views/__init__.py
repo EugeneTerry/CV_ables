64d9fb48273cbae8d15a62a77083da67916f8aa2
Eugene Terry <eugene.terry73@gmail.com>
2022-03-15 21:40:17 -0500

    finished all views

--- CV_ables_api/views/__init__.py
+++ CV_ables_api/views/__init__.py
@@ -2,6 +2,7 @@ from .auth import login_user, register_user
 from .applicants import get_profile
 from .descriptions import DescriptionView
 from .educations import EducationView
+from .education_vitas import EducationVitaView
 from .languages import LanguageView
 from .jobtypes import JobtypeView
 from .missions import MissionView
@@ -10,3 +11,6 @@ from .projects import ProjectView
 from .prospects import ProspectView
 from .vitas import VitaView
 from .experiences import ExerienceView
+from .experience_vitas import ExperienceVitaView
+from .experience_frames import ExperienceFrameView
+from .experience_langs import ExperienceLangView
